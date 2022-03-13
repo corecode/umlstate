5706184b8afed84e83cde279af81256ecb91e634
Simon Schubert <2@0x2c.org>
2022-03-13 23:19:16 +0100

    prototype: put everything into a module

--- tests/prototype.rs
+++ tests/prototype.rs
@@ -5,63 +5,69 @@ trait EventProcessor<E> {
 struct EventA;
 struct EventB(u32);
 
-enum MyMachineState {
-    State1,
-    State2,
-}
+mod MyMachineMod {
+    use super::*;
 
-enum MyMachineEvent {
-    EventA(EventA),
-    EventB(EventB),
-}
-
-struct MyMachine {
-    context: MyMachineContext,
-    state: MyMachineState,
-}
-
-impl MyMachine {
-    pub fn new(context: MyMachineContext) -> Self {
-        MyMachine {
-            context,
-            state: MyMachineState::State1,
-        }
+    pub enum State {
+        State1,
+        State2,
     }
 
-    fn process_internal(&mut self, event: MyMachineEvent) {
-        match self.state {
-            MyMachineState::State1 => match &event {
-                MyMachineEvent::EventA(_event) => {
-                    self.state = MyMachineState::State2;
-                }
-                _ => (),
-            },
-            MyMachineState::State2 => match &event {
-                MyMachineEvent::EventB(event) => {
-                    let ctx = &self.context;
-                    if ctx.is_even_p(event) {
-                        let ctx = &mut self.context;
-                        ctx.on_b();
-                        self.state = MyMachineState::State1;
+    enum Event {
+        EventA(EventA),
+        EventB(EventB),
+    }
+
+    pub(crate) struct Machine {
+        context: MyMachineContext,
+        state: State,
+    }
+
+    impl Machine {
+        pub fn new(context: MyMachineContext) -> Self {
+            Machine {
+                context,
+                state: State::State1,
+            }
+        }
+
+        fn process_internal(&mut self, event: Event) {
+            match self.state {
+                State::State1 => match &event {
+                    Event::EventA(_event) => {
+                        self.state = State::State2;
                     }
-                }
-                _ => (),
-            },
+                    _ => (),
+                },
+                State::State2 => match &event {
+                    Event::EventB(event) => {
+                        let ctx = &self.context;
+                        if ctx.is_even_p(event) {
+                            let ctx = &mut self.context;
+                            ctx.on_b();
+                            self.state = State::State1;
+                        }
+                    }
+                    _ => (),
+                },
+            }
+        }
+    }
+
+    impl EventProcessor<EventA> for Machine {
+        fn process(&mut self, event: EventA) {
+            self.process_internal(Event::EventA(event));
+        }
+    }
+
+    impl EventProcessor<EventB> for Machine {
+        fn process(&mut self, event: EventB) {
+            self.process_internal(Event::EventB(event));
         }
     }
 }
 
-impl EventProcessor<EventA> for MyMachine {
-    fn process(&mut self, event: EventA) {
-        self.process_internal(MyMachineEvent::EventA(event));
-    }
-}
-
-impl EventProcessor<EventB> for MyMachine {
-    fn process(&mut self, event: EventB) {
-        self.process_internal(MyMachineEvent::EventB(event));
-    }
-}
+use MyMachineMod::Machine as MyMachine;
 
 struct MyMachineContext;
 
