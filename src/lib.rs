ffd64d3920ad789829c00e14cdddd3fd274507cc
Simon Schubert <2@0x2c.org>
2022-03-19 22:54:55 +0100

    refactor macros into sub-crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,1 @@
-use syn::*;
-
-mod analyze;
-mod codegen;
-mod parse;
-
-#[proc_macro]
-pub fn umlstate(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let ast = parse_macro_input!(input as parse::UmlState);
-    let model = analyze::analyze(ast);
-    let model = match model {
-        Err(err) => return err.into_compile_error().into(),
-        Ok(model) => model,
-    };
-
-    codegen::generate(model).into()
-}
+pub use umlstate_macros::umlstate;
