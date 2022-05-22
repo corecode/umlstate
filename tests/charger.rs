8eec8c282829df67f026509fcb360e957e8aa423
Simon Schubert <2@0x2c.org>
2022-05-22 18:33:49 +0200

    add charger test example

--- tests/charger.rs
+++ tests/charger.rs
@@ -1,28 +1,62 @@
-// use umlstate::*;
+use umlstate::*;
 
-// umlstate! {
-//     machine ChargePower {
-//         event UsbConnected;
-//         event UsbDisconnected;
-//         event ChargeActive;
-//         event ChargeInactive;
+#[derive(Clone)]
+struct UsbConnected;
+#[derive(Clone)]
+struct UsbDisconnected;
+#[derive(Clone)]
+struct ChargeActive;
+#[derive(Clone)]
+struct ChargeInactive;
 
-//         state Unpowered;
-//         machine Powered {
-//             state WaitCharge;
-//             state Charging;
-//             state ChargeDone;
+umlstate! {
+    machine ChargePower {
+        state Unpowered;
 
-//             initial -> WaitCharge;
-//             WaitCharge + ChargeActive -> Charging;
-//             Charging + ChargeInactive -> ChargeDone;
-//         }
+        machine Powered {
+            state WaitCharge;
+            state Charging;
+            state ChargeDone;
 
-//         initial -> Unpowered / use_power(Power::Battery);
-//         Unpowered + UsbConnected -> Powered / use_power(Power::Usb);
-//         Powered + UsbDisconnected -> Unpowered / use_power(Power::Battery);
-//     }
-// }
+            //initial -> WaitCharge;
+            WaitCharge + ChargeActive => Charging / indicate(Charge::Starting);
+            Charging + ChargeInactive => ChargeDone / indicate(Charge::Done);
+        }
+
+        //initial -> Unpowered / use_power(Power::Battery);
+        Unpowered + UsbConnected => Powered / use_power(Power::Usb);
+        Powered + UsbDisconnected => Unpowered / use_power(Power::Battery);
+    }
+}
+
+#[derive(Debug)]
+enum Power {
+    Battery,
+    Usb,
+}
+
+#[derive(Debug)]
+enum Charge {
+    Starting,
+    Done,
+}
+
+fn use_power(source: Power) {
+    println!("using power from {:?}", source);
+}
+
+fn indicate(charge: Charge) {
+    println!("now {:?} charging", charge);
+}
 
 #[test]
-fn compiles() {}
+fn charger() {
+    let mut charge_logic = ChargePower::new();
+    charge_logic.enter();
+    charge_logic.process(UsbConnected);
+    charge_logic.process(ChargeActive);
+    charge_logic.process(ChargeInactive);
+    charge_logic.process(ChargeActive);
+    charge_logic.process(UsbDisconnected);
+    charge_logic.process(ChargeInactive);
+}
